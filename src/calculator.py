098b300b78d0eca3f6673973d155fc40f5e84b12
kyesteele <kss25@hawaii.edu>
2025-09-29 21:45:42 -1000

    Basic calculator functions and unit tests.

---
+++ src/calculator.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def divide(a, b):
+    try:
+        quotient = a / b
+        return quotient
+    except ZeroDivisionError:
+        print("Error -- dividing by zero.")
+        return None
